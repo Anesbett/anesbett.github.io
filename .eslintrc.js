c45db280404845badecf41ffc17fcd9fd5d1da5a
Andrew Nesbett <anesbett@gmail.com>
2024-07-17 10:54:07 -0400

    upadted EOL

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-const os = require("os");
+const os = require('os');
 
 module.exports = {
   env: {
@@ -7,46 +6,46 @@ module.exports = {
     jest: true,
     node: true,
   },
-  extends: "airbnb",
-  ignorePatterns: ["node_modules/", "build/"],
-  parser: "@babel/eslint-parser",
-  plugins: ["react"],
+  extends: 'airbnb',
+  ignorePatterns: ['node_modules/', 'build/'],
+  parser: '@babel/eslint-parser',
+  plugins: ['react'],
   rules: {
     quotes: [2, "double", { avoidEscape: true }],
-    "linebreak-style": ["error", "windows"],
-    "jsx-a11y/anchor-is-valid": ["error", {
-      components: ["Link"],
-      specialLink: ["to", "hrefLeft", "hrefRight"],
-      aspects: ["noHref", "invalidHref", "preferButton"],
-      "linebreak-style": 0,
-      "global-require": 0,
-      "eslint linebreak-style": [0, "error", "windows"],
-    }],
-    "jsx-a11y/click-events-have-key-events": 0,
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/no-static-element-interactions": 0,
-    "no-console": [
-      "error",
+    'jsx-a11y/anchor-is-valid': [
+      'error',
       {
-        allow: ["warn", "error", "info"],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+        components: ['Link'],
+        specialLink: ['to', 'hrefLeft', 'hrefRight'],
       },
     ],
-    "no-underscore-dangle": 0,
-    "react/destructuring-assignment": 0,
-    "react/function-component-definition": [
-      2,
-      { namedComponents: "arrow-function" },
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/no-static-element-interactions': 0,
+    'linebreak-style': ['error', os.EOL === '\r\n' ? 'windows' : 'unix'],
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'info'],
+      },
     ],
-    "react/jsx-filename-extension": [
+    'no-underscore-dangle': 0,
+    'react/destructuring-assignment': 0,
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".js", ".jsx"],
+        extensions: ['.js', '.jsx'],
       },
     ],
-    "react/jsx-no-useless-fragment": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/jsx-wrap-multilines": [
+    'react/jsx-no-useless-fragment': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-wrap-multilines': [
       1,
       {
         assignment: true,
